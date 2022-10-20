e5eda5e4bd7122883f358a4af5f6d78aec75f33c
marvin255 <postbox433@gmail.com>
2022-10-20 19:39:28 +0200

    Locale (#3)

---
+++ tests/Locale/LocaleValueTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\DoctrineTranslation\Tests\Locale;
+
+use InvalidArgumentException;
+use Marvin255\DoctrineTranslation\Locale\LocaleValue;
+use Marvin255\DoctrineTranslation\Tests\BaseCase;
+
+/**
+ * @internal
+ */
+class LocaleValueTest extends BaseCase
+{
+    public function testEmptyConstructorValue(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new LocaleValue('');
+    }
+
+    public function testSpacesOnlyConstructorValue(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new LocaleValue('       ');
+    }
+
+    /**
+     * @dataProvider provideGetLanguage
+     */
+    public function testGetLanguage(string $locale, string $reference): void
+    {
+        $localeValue = new LocaleValue($locale);
+        $language = $localeValue->getLanguage();
+
+        $this->assertSame($reference, $language);
+    }
+
+    public function provideGetLanguage(): array
+    {
+        return [
+            'only language' => [
+                'en',
+                'en',
+            ],
+            'with region' => [
+                'en-US',
+                'en',
+            ],
+            'wrong case' => [
+                'eN-US',
+                'en',
+            ],
+            'wrong space in the middle' => [
+                'en -US',
+                'en',
+            ],
+            'wrong space in the beginning' => [
+                ' en-US',
+                'en',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideGetRegion
+     */
+    public function testGetRegion(string $locale, string $reference): void
+    {
+        $localeValue = new LocaleValue($locale);
+        $region = $localeValue->getRegion();
+
+        $this->assertSame($reference, $region);
+    }
+
+    public function provideGetRegion(): array
+    {
+        return [
+            'only language' => [
+                'en',
+                '',
+            ],
+            'with region' => [
+                'en-US',
+                'US',
+            ],
+            'wrong case' => [
+                'en-Us',
+                'US',
+            ],
+            'wrong space in the middle' => [
+                'en- US',
+                'US',
+            ],
+            'wrong space in the end' => [
+                'en-US ',
+                'US',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideGetFull
+     */
+    public function testGetFull(string $locale, string $reference): void
+    {
+        $localeValue = new LocaleValue($locale);
+        $full = $localeValue->getFull();
+
+        $this->assertSame($reference, $full);
+    }
+
+    public function provideGetFull(): array
+    {
+        return [
+            'only language' => [
+                'en',
+                'en',
+            ],
+            'with region' => [
+                'en-US',
+                'en-US',
+            ],
+            'wrong case' => [
+                'eN-Us',
+                'en-US',
+            ],
+            'wrong space in the middle' => [
+                'en - US',
+                'en-US',
+            ],
+            'wrong space in the end' => [
+                ' en-US ',
+                'en-US',
+            ],
+        ];
+    }
+}
