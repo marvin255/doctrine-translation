c8edc987582a4c79fa4f69fb37b9c41f3ad0e935
marvin255 <postbox433@gmail.com>
2022-10-20 19:37:00 +0200

    Add locale

---
+++ src/Locale/Locale.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\DoctrineTranslation\Locale;
+
+/**
+ * Interface for object that represents language locale.
+ */
+interface Locale
+{
+    /**
+     * Returns primary language from locale. E.g. returns en from en-US.
+     *
+     * @return string
+     */
+    public function getLanguage(): string;
+
+    /**
+     * Returns region from locale or empty string if there is no region. E.g. returns US from en-US.
+     *
+     * @return string
+     */
+    public function getRegion(): string;
+
+    /**
+     * Returns full locale name including language and region.
+     *
+     * @return string
+     */
+    public function getFull(): string;
+}
