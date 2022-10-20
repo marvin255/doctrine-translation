b9fbdf2120d4ea844ab36b7e45413feff5999761
marvin255 <postbox433@gmail.com>
2022-10-20 16:53:24 +0200

    Basic structure (#1)

---
+++ src/Marvin255DoctrineTranslationBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\DoctrineTranslation;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * Bundle object.
+ */
+class Marvin255DoctrineTranslationBundle extends AbstractBundle
+{
+}
