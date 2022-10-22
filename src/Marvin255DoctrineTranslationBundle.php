fbe9964f5040274ad7c026246f57f41de310c025
marvin255 <postbox433@gmail.com>
2022-10-22 21:56:47 +0200

    Add correct config for new doctrine type

--- src/Marvin255DoctrineTranslationBundle.php
+++ src/Marvin255DoctrineTranslationBundle.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Marvin255\DoctrineTranslationBundle;
 
+use Marvin255\DoctrineTranslationBundle\Locale\LocaleType;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 /**
@@ -11,8 +14,34 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
  */
 class Marvin255DoctrineTranslationBundle extends AbstractBundle
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $bundles = $builder->getParameter('kernel.bundles');
+
+        if (!isset($bundles['DoctrineBundle'])) {
+            return;
+        }
+
+        $builder->prependExtensionConfig(
+            'doctrine',
+            [
+                'dbal' => [
+                    'types' => [
+                        LocaleType::LOCALE_TYPE => LocaleType::class,
+                    ],
+                ],
+            ]
+        );
     }
 }
