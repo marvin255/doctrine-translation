5d75e28e013846ea953c55b1d09df80b35654529
marvin255 <postbox433@gmail.com>
2022-11-28 16:59:13 +0100

    Move current and default locales to special provider

---
+++ src/Locale/LocaleProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\DoctrineTranslationBundle\Locale;
+
+use Symfony\Contracts\Translation\LocaleAwareInterface;
+
+/**
+ * Object that can provide current locale and default locale from Symfony settings.
+ */
+class LocaleProvider
+{
+    private readonly LocaleAwareInterface $localeSwitcher;
+
+    private readonly string $defaultLocaleString;
+
+    private ?Locale $defaultLocale = null;
+
+    public function __construct(LocaleAwareInterface $localeSwitcher, ?string $defaultLocaleString = null)
+    {
+        $this->localeSwitcher = $localeSwitcher;
+        $this->defaultLocaleString = (string) $defaultLocaleString;
+    }
+
+    /**
+     * Return object that represents current locale.
+     */
+    public function getCurrentLocale(): Locale
+    {
+        $currentLocaleString = $this->localeSwitcher->getLocale();
+
+        if ($currentLocaleString === '') {
+            throw new \RuntimeException("Current locale wasn't provided");
+        }
+
+        return LocaleFactory::create($currentLocaleString);
+    }
+
+    /**
+     * Return object that represents default locale.
+     */
+    public function getDefaultLocale(): Locale
+    {
+        if ($this->defaultLocaleString === '') {
+            throw new \RuntimeException("Default locale wasn't provided");
+        }
+
+        if ($this->defaultLocale === null) {
+            $this->defaultLocale = LocaleFactory::create($this->defaultLocaleString);
+        }
+
+        return $this->defaultLocale;
+    }
+}
---
+++ tests/Locale/LocaleProviderTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\DoctrineTranslationBundle\Tests\Locale;
+
+use Marvin255\DoctrineTranslationBundle\Locale\LocaleProvider;
+use Marvin255\DoctrineTranslationBundle\Tests\BaseCase;
+use Symfony\Contracts\Translation\LocaleAwareInterface;
+
+/**
+ * @internal
+ */
+class LocaleProviderTest extends BaseCase
+{
+    /**
+     * @test
+     */
+    public function testGetCurrentLocale(): void
+    {
+        $locale = 'en';
+        $switcher = $this->createLocaleSwitcherMock($locale);
+
+        $provider = new LocaleProvider($switcher);
+
+        $this->assertSame($locale, $provider->getCurrentLocale()->getFull());
+    }
+
+    /**
+     * @test
+     */
+    public function testGetCurrentLocaleEmptyLocale(): void
+    {
+        $switcher = $this->createLocaleSwitcherMock('');
+
+        $provider = new LocaleProvider($switcher);
+
+        $this->expectException(\RuntimeException::class);
+        $provider->getCurrentLocale();
+    }
+
+    /**
+     * @test
+     */
+    public function testGetDefaultLocale(): void
+    {
+        $switcher = $this->createLocaleSwitcherMock();
+        $defaultLocale = 'en';
+
+        $provider = new LocaleProvider($switcher, $defaultLocale);
+
+        $this->assertSame($defaultLocale, $provider->getDefaultLocale()->getFull());
+    }
+
+    /**
+     * @test
+     */
+    public function testGetDefaultLocaleEmptyLocale(): void
+    {
+        $switcher = $this->createLocaleSwitcherMock();
+        $defaultLocale = '';
+
+        $provider = new LocaleProvider($switcher, $defaultLocale);
+
+        $this->expectException(\RuntimeException::class);
+        $provider->getDefaultLocale();
+    }
+
+    private function createLocaleSwitcherMock(?string $locale = null): LocaleAwareInterface
+    {
+        /** @var LocaleAwareInterface&MockObject */
+        $localeSwitcher = $this->getMockBuilder(LocaleAwareInterface::class)->getMock();
+
+        if ($locale === null) {
+            $localeSwitcher->expects($this->never())->method('getLocale');
+        } else {
+            $localeSwitcher->method('getLocale')->willReturn($locale);
+        }
+
+        return $localeSwitcher;
+    }
+}
